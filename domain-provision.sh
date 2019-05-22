5dd4278fe7d39c7071cb033b8156f4cb2e09a580
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-05-22 12:44:07 -0300

    write krb5.conf only if not have domain

--- domain-provision.sh
+++ domain-provision.sh
@@ -5,9 +5,11 @@ samba-tool domain provision \
   --realm=${REALM} --domain=${DOMAIN} --adminpass=${ADMIN_MASS} \
   --option="dns forwarder=${DNS_FORWARDER}"
 
-cat /usr/local/samba/private/krb5.conf > /etc/krb5.conf
+if ! grep -q ${DOMAIN} /etc/krb5.conf > /dev/null 2>&1; then
+  cat /usr/local/samba/private/krb5.conf > /etc/krb5.conf
+fi
 
-if ! grep -q samdom /etc/resov.conf > /dev/null 2>&1; then 
+if ! grep -q ${SEARCH_DOMAIN} /etc/resov.conf > /dev/null 2>&1; then
   echo -e "namerserver ${SERVER_IP}\nsearch ${SEARCH_DOMAIN}" > /etc/resolv.conf
 fi
 
