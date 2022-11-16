911582604deab03ceac14433d0bc69853846b77a
Diego Gobbi Slomp <diegogslomp@gmail.com>
2022-11-16 13:23:18 -0300

    add insecure LDAP as default, add LDAP queries to samba-tests, upload only almalinux to dockerhub

--- dev/update-tags-and-push-to-dockerhub.sh
+++ dev/update-tags-and-push-to-dockerhub.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 repo="diegogslomp/samba-ad-dc"
 
 # Update ubuntu, debian and rockylinux tags
-for tag in ubuntu debian rockylinux; do
-  docker tag "samba:${tag}" "${repo}:${tag}"
-  docker push "${repo}:${tag}"
-  docker rmi "${repo}:${tag}"
-done
+# for tag in ubuntu debian rockylinux; do
+#   docker tag "samba:${tag}" "${repo}:${tag}"
+#   docker push "${repo}:${tag}"
+#   docker rmi "${repo}:${tag}"
+# done
 
 # Get samba version from last almalinux
 version=$(docker run --rm samba:almalinux samba --version | awk '{ print $2 }')
+
 # Update almalinux (main) tags
 for tag in almalinux "${version}" latest; do
   docker tag samba:almalinux "${repo}:${tag}"
