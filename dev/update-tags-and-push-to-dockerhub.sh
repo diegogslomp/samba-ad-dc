9da3da85c7f80075950f1ead5efb43f7405f1cb1
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-03-10 16:45:41 -0300

    Add arm64 build to scripts

--- dev/update-tags-and-push-to-dockerhub.sh
+++ dev/update-tags-and-push-to-dockerhub.sh
@@ -7,12 +7,21 @@ repo="diegogslomp/samba-ad-dc"
 # Get samba version from last almalinux
 version=$(docker run --rm samba:almalinux samba --version | awk '{ print $2 }')
 
+# Update arm64
+docker tag samba:arm64 "${repo}:arm64"
+docker push "${repo}:arm64"
+
 # Update almalinux (main) tags
-for tag in "${version}" almalinux latest; do
+for tag in amd64 almalinux "${version}"; do
   docker tag samba:almalinux "${repo}:${tag}"
   docker push "${repo}:${tag}"
   docker rmi "${repo}:${tag}"
 done
+
+# Update manifest to accept amd64 and arm64 as latest
+docker manifest rm diegogslomp/samba-ad-dc:latest
+docker manifest create diegogslomp/samba-ad-dc:latest diegogslomp/samba-ad-dc:amd64 diegogslomp/samba-ad-dc:arm64
+docker manifest push diegogslomp/samba-ad-dc:latest
 
 # Cleanup
 docker image prune -f
