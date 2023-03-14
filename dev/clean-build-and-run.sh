10e049935d7e679d9b337c9ff4aea6ae0f55b4f8
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-03-14 08:25:18 -0300

    Comment arm64 build

--- dev/clean-build-and-run.sh
+++ dev/clean-build-and-run.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 set -x
 
 docker compose down -v
-docker build --platform="linux/arm64" -f dockerfiles/almalinux --load --tag samba:arm64 .
+# docker build --platform="linux/arm64" -f dockerfiles/almalinux --load --tag samba:arm64 .
 docker compose build
 docker compose up -d
 docker compose logs -f
--- dev/update-tags-and-push-to-dockerhub.sh
+++ dev/update-tags-and-push-to-dockerhub.sh
@@ -8,21 +8,21 @@ repo="diegogslomp/samba-ad-dc"
 version=$(docker run --rm samba:almalinux samba --version | awk '{ print $2 }')
 
 # Update arm64
-docker tag samba:arm64 "${repo}:arm64"
-docker push "${repo}:arm64"
-docker rmi "${repo}:arm64"
+# docker tag samba:arm64 "${repo}:arm64"
+# docker push "${repo}:arm64"
+# docker rmi "${repo}:arm64"
 
 # Update almalinux (main) tags
-for tag in amd64 almalinux "${version}"; do
+for tag in amd64 almalinux "${version}" latest; do
   docker tag samba:almalinux "${repo}:${tag}"
   docker push "${repo}:${tag}"
   docker rmi "${repo}:${tag}"
 done
 
 # Update manifest to accept amd64 and arm64 as latest
-docker manifest rm diegogslomp/samba-ad-dc:latest
-docker manifest create diegogslomp/samba-ad-dc:latest diegogslomp/samba-ad-dc:amd64 diegogslomp/samba-ad-dc:arm64
-docker manifest push diegogslomp/samba-ad-dc:latest
+# docker manifest rm diegogslomp/samba-ad-dc:latest
+# docker manifest create diegogslomp/samba-ad-dc:latest diegogslomp/samba-ad-dc:amd64 diegogslomp/samba-ad-dc:arm64
+# docker manifest push diegogslomp/samba-ad-dc:latest
 
 # Cleanup
 docker image prune -f
