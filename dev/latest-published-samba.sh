6dd4ba13ccc99da5540a262a64ecdfb3b4abbc7b
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-03-21 16:16:10 -0300

    Fix latest published samba script

--- dev/latest-published-samba.sh
+++ dev/latest-published-samba.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl -sL https://download.samba.org/pub/samba |
-  grep LATEST |
-  awk -F\" '{ print $12 }' |
-  awk -F- '{ print $4 }'
+curl -sL https://wiki.samba.org/index.php/Main_Page |
+  grep "Current_Stable_Release" |
+  awk -F ">" '{print $8}' |
+  awk -F "<" '{print $1}'
