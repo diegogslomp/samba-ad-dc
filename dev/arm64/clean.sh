5c22dc193f92b12041b54ab21dc25ec6dc2f3f43
Diego Gobbi Slomp <diegogslomp@gmail.com>
2024-06-25 08:07:24 -0300

    Remove deprecated version from compose files, adjust arm build to new volume changes

--- dev/arm64/clean.sh
+++ dev/arm64/clean.sh
@@ -4,4 +4,4 @@ set -x
 
 docker stop dc1 || true
 docker rm dc1 || true
-docker volume rm dc1-samba || true
+docker volume rm dc1-etc dc1-private dc1-var || true
--- dev/arm64/run.sh
+++ dev/arm64/run.sh
@@ -4,7 +4,7 @@ set -x
 
 docker stop dc1 || true
 docker rm dc1 || true
-docker volume rm dc1-samba || true
+docker volume rm dc1-etc dc1-private dc1-var || true
 
 docker run -d --privileged \
   --restart=unless-stopped \
@@ -14,7 +14,9 @@ docker run -d --privileged \
   -e DOMAIN='DGS' \
   -e ADMIN_PASS='Passw0rd' \
   -e DNS_FORWARDER='8.8.8.8' \
-  -v dc1-samba:/usr/local/samba \
+  -v dc1-etc:/usr/local/samba/etc \
+  -v dc1-private:/usr/local/samba/private \
+  -v dc1-var:/usr/local/samba/var \
   --name dc1 --hostname DC1 samba:arm64
 
 docker logs dc1 -f
